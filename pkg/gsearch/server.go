a25707d57cc86f2e2606d3f274b17981272a118b
Juergen Enge <juergen@info-age.net>
2020-08-31 18:36:07 +0200

    printstack added

--- pkg/gsearch/server.go
+++ pkg/gsearch/server.go
@@ -39,6 +39,7 @@ import (
 	"net/url"
 	"os"
 	"regexp"
+	"runtime/debug"
 	"sort"
 	"strings"
 	"time"
@@ -541,6 +542,7 @@ func (s *Server) DoPanicJSON(writer http.ResponseWriter, status int, message str
 		StatusText string `json:"statustext"`
 		Message    string `json:"message"`
 	}
+	debug.PrintStack()
 	s.log.Error(message)
 	data := errData{
 		Status:     status,
